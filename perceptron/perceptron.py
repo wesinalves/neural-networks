927e033937bbfbc8b4bab38c788fb43dd3815364
wesinalves <wesinec@gmail.com>
2019-01-25 14:09:57 -0200

    update perceptron

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -14,8 +14,6 @@ x_n = np.concatenate([(sigma_a * np.random.randn(n_samples//2) + mean_a), (sigma
 x_test = np.concatenate([(sigma_a * np.random.randn(n_samples//3) + mean_a), (sigma_b * np.random.randn(n_samples//3) + mean_b) ], axis=None)
 
 w_n = np.zeros(len(x_n))
-
-y_n = np.zeros(len(x_n))
 y_test = np.zeros(len(x_test))
 
 line = 2*x_n + 0.3
@@ -23,7 +21,7 @@ line_test = 2*x_test + 0.3
 
 
 
-neta = 0.3
+neta = 0.1
 
 timesteps = 10
 
@@ -48,22 +46,33 @@ def sigum(value):
 d_n = quantizer(line, np.mean(line))
 d_test = quantizer(line_test, np.mean(line_test))
 
+cost = []
 
-for n in range(len(x_n)-1):
-	y_n[n] = sigum(np.dot(w_n.T[n],x_n[n]))
-	w_n[n+1] = w_n[n] + neta*(d_n[n] - y_n[n])*x_n[n]
+for i in range(timesteps):
+	y_n = np.zeros(len(x_n))
+	for n in range(len(x_n)-1):
+		y_n[n] = sigum(np.dot(w_n.T[n],x_n[n]))
+		print(y_n[n])	
+		w_n[n+1] = w_n[n] + neta*(d_n[n] - y_n[n])*x_n[n]
+
+	cost.append(np.sum((d_n - y_n)/len(y_n))**2)
 
 
-mse = np.zeros(len(w_n))
-for i in range(len(w_n)):
-	for n in range(len(x_test)):
-		y_test[n] = sigum(np.dot(w_n.T[i],x_test[n]))
+for n in range(len(x_test)-1):
+	y_test[n] = sigum(np.dot(w_n.T[-1],x_test[n]))
 
-	mse[i] = (np.sum(d_test - y_test)/len(y_test))**2
+mse = (np.sum(d_test - y_test)/len(y_test))**2
 
 plt.figure(1)
-plt.plot(mse, label='mse')
+plt.plot(cost, label='loss')
 plt.figure(2)
 plt.scatter(x_n, line)
 plt.legend()
+plt.figure(3)
+plt.plot(w_n)
 plt.show()
+
+'''
+print(d_n)
+print(y_n)
+'''
